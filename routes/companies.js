b4d72a8b585aa448df8dbf199a2217ae037a016f
Olivia Golden <olivia.golden.w@gmail.com>
2024-02-21 14:07:58 -0600

    update doc authorization

--- routes/companies.js
+++ routes/companies.js
@@ -93,7 +93,7 @@ router.get("/:handle", async function (req, res, next) {
  *
  * Returns { handle, name, description, numEmployees, logo_url }
  *
- * Authorization required: login
+ * Authorization required: admin
  */
 
 router.patch("/:handle", ensureAdmin, async function (req, res, next) {
@@ -113,7 +113,7 @@ router.patch("/:handle", ensureAdmin, async function (req, res, next) {
 
 /** DELETE /[handle]  =>  { deleted: handle }
  *
- * Authorization: login
+ * Authorization: admin
  */
 
 router.delete("/:handle", ensureAdmin, async function (req, res, next) {
